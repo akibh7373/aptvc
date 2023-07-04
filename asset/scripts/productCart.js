cb336ed376146deee0822ea5ed81f5fa1809d5e3
Akib Hossain <akib.hossain737@gmail.com>
2023-07-04 02:34:26 -0700

    v1.0.1

--- asset/scripts/productCart.js
+++ asset/scripts/productCart.js
@@ -27,11 +27,11 @@ const navWrapper = document.querySelector('.product_nav_wrapper');
 navBtn.addEventListener('click', () => {
     navWrapper.classList.toggle('product_nav_wrapper_active');
 });
-for (let i = 0; i < cart.length; i++) {
+cart.map(({ image, title }) => {
     navWrapper.innerHTML += `
-    <a href="${cart[i].title}.html" class="product_nav_items">
-        <img src="/src/images/products/${cart[i].image}" alt="" class="product_nav_img">
-        <h6 class="product_nav_title mt-lg-2 text-uppercase text-dark">${cart[i].title}</h6>
+    <a href="${title}.html" class="product_nav_items">
+        <img src="/src/images/products/${image}" alt="" class="product_nav_img">
+        <h6 class="product_nav_title mt-lg-2 text-uppercase text-dark">${title}</h6>
     </a>
     `
-}
+})
